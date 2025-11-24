4cbf97e2679d50f9f7ff7defc3ce93e6a582fee5
wangjiawei1995 <zhanshen02154@gmail.com>
2025-11-24 12:43:09 +0800

    refactor(健康检查服务器): 移动到基础设施层的server包

--- internal/infrastructure/server/health_check.go
+++ internal/infrastructure/server/health_check.go
@@ -1,22 +1,23 @@
-package infrastructure
+package server
 
 import (
 	"context"
+	"github.com/zhanshen02154/order/internal/infrastructure"
 	"go-micro.dev/v4/logger"
 	"net/http"
 	"sync"
 	"sync/atomic"
 )
 
-// 健康检查探针
+// ProbeServer 健康检查探针
 type ProbeServer struct {
 	server         *http.Server
 	wg             sync.WaitGroup
 	isShuttingDown atomic.Bool
-	serviceContext *ServiceContext
+	serviceContext *infrastructure.ServiceContext
 }
 
-func NewProbeServer(port string, serviceContext *ServiceContext) *ProbeServer {
+func NewProbeServer(port string, serviceContext *infrastructure.ServiceContext) *ProbeServer {
 	mx := http.NewServeMux()
 	mx.HandleFunc("/healthz", func(writer http.ResponseWriter, request *http.Request) {
 		writer.WriteHeader(http.StatusOK)
@@ -38,7 +39,7 @@ func NewProbeServer(port string, serviceContext *ServiceContext) *ProbeServer {
 	}
 }
 
-// 启动服务器
+// Start 启动服务器
 func (probeServe *ProbeServer) Start() error {
 	probeServe.wg.Add(1)
 	go func() {
@@ -51,7 +52,7 @@ func (probeServe *ProbeServer) Start() error {
 	return nil
 }
 
-// 关闭服务器
+// Shutdown 关闭服务器
 func (probeServe *ProbeServer) Shutdown(ctx context.Context) error {
 	probeServe.isShuttingDown.Store(true)
 	if err := probeServe.server.Shutdown(ctx); err != nil {
