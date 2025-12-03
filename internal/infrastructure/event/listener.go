13e81c9eb3148e0f3cb6bceb28bbab3b3e39f809
wangjiawei1995 <zhanshen02154@gmail.com>
2025-12-03 15:02:08 +0800

    refactor: 调整目录结构
    优化目录结构，基础设施层移除的目录包括：
    - broker
    - config
    - server
    - registory
    
    resolves #64, #65, #66, #68, zhanshen02154/go-micro-service#15
    
    BREAKING CHANGE: 基础设施层移除的目录：
    - broker
    - config
    - server
    - registory

--- internal/infrastructure/event/listener.go
+++ internal/infrastructure/event/listener.go
@@ -2,79 +2,23 @@ package event
 
 import (
 	"context"
-	"fmt"
-	"go-micro.dev/v4"
+	"github.com/zhanshen02154/order/internal/config"
 	"go-micro.dev/v4/client"
-	"go-micro.dev/v4/logger"
-	"sync"
 )
 
-// 侦听器
-type MicroListener struct {
-	mu sync.RWMutex
-	eventPublisher map[string]micro.Event
-	c client.Client
+// 事件总线
+type Listener interface {
+	Publish(ctx context.Context, topic string, event interface{}, opts ...client.PublishOption) error
+	Register(topic string) bool
+	UnRegister(topic string) bool
+	Close()
 }
 
-// 发布
-func (l *MicroListener) Publish(ctx context.Context, topic string, msg interface{}, opts ...client.PublishOption) error {
-	l.mu.RLock()
-	defer l.mu.RUnlock()
-	if _, ok := l.eventPublisher[topic]; !ok {
-		return fmt.Errorf("topic: %s event not registerd", topic)
-	}
-	return l.eventPublisher[topic].Publish(ctx, msg, opts...)
-}
-
-// 注册
-func (l *MicroListener) Register(topic string) bool {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	if _, ok := l.eventPublisher[topic]; !ok {
-		l.eventPublisher[topic] = micro.NewEvent(topic, l.c)
-	}
-	logger.Info("event ", topic, " was registered")
-	return true
-}
-
-// 取消注册
-func (l *MicroListener) UnRegister(topic string) bool {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	if l.eventPublisher == nil {
-		return true
-	}
-	if len(l.eventPublisher) == 0 {
-		return true
-	}
-	if _, ok := l.eventPublisher[topic]; ok {
-		delete(l.eventPublisher, topic)
-		logger.Info("event: ", topic, " unregistered")
-	}
-	return true
-}
-
-// 关闭
-func (l *MicroListener) Close() {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	if l.eventPublisher == nil {
-		return
-	}
-	if len(l.eventPublisher) > 0 {
-		return
-	}
-	for k, _ := range l.eventPublisher {
-		delete(l.eventPublisher, k)
-		logger.Info("event: ", k, " unregistered")
-	}
-}
-
-// 新建侦听器
-func NewListener(c client.Client) Bus {
-	return &MicroListener{
-		mu:             sync.RWMutex{},
-		eventPublisher: make(map[string]micro.Event),
-		c:              c,
+// 注册发布事件
+func RegisterPublisher(conf *config.Broker, eb Listener) {
+	if len(conf.Publisher) > 0 {
+		for i := range conf.Publisher {
+			eb.Register(conf.Publisher[i])
+		}
 	}
 }
