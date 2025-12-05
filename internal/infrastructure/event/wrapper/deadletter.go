d29033336eff2ae512811098296b840d4dcace72
wangjiawei1995 <zhanshen02154@gmail.com>
2025-12-05 17:20:51 +0800

    feat: 新增响应代码判断是否移入死信队列
    resolves #64, #65, #66, #68, zhanshen02154/go-micro-service#15

--- internal/infrastructure/event/wrapper/deadletter.go
+++ internal/infrastructure/event/wrapper/deadletter.go
@@ -19,34 +19,41 @@ func (w *DeadLetterWrapper) Wrapper() server.SubscriberWrapper {
 	return func(next server.SubscriberFunc) server.SubscriberFunc {
 		return func(ctx context.Context, msg server.Message) error {
 			err := next(ctx, msg)
-			logger.Info("aaaaa")
-			if err != nil {
-				errStatus, ok := status.FromError(err)
-				if !ok {
-					logger.Errorf("failed to handler topic %v, error: %s; id: %s", msg.Topic(), err.Error(), msg.Header()["Micro-ID"])
-					return err
-				}
-				switch errStatus.Code() {
-				case codes.InvalidArgument:
-					return err
-				case codes.Aborted:
-					return err
-				case codes.DataLoss:
-					return nil
-				}
-
-				logger.Error("failed to consume event: ", err)
-				dlMsg := &broker.Message{
-					Header: msg.Header(),
-					Body:   msg.Body(),
-				}
-				dlMsg.Header["error"] = err.Error()
-				topic := msg.Topic() + "-DLQ"
-				if err := w.b.Publish(msg.Topic() + "-DLQ", dlMsg); err != nil {
-					logger.Errorf("failed to publish to %s, error: %s", topic, err.Error())
-					return nil
-				}
+			logger.Info("error info: ", err)
+			if err == nil {
+				return nil
 			}
+			errStatus, ok := status.FromError(err)
+			if !ok {
+				logger.Errorf("failed to handler topic %v, error: %s; id: %s", msg.Topic(), err.Error(), msg.Header()["Micro-ID"])
+				return err
+			}
+			switch errStatus.Code() {
+			case codes.InvalidArgument:
+				return err
+			case codes.DataLoss:
+				return err
+			case codes.PermissionDenied:
+				return err
+			case codes.Unauthenticated:
+				return err
+			case codes.Aborted:
+				return err
+			case codes.NotFound:
+				return err
+			}
+
+			logger.Info("开始执行死信队列")
+			dlMsg := &broker.Message{
+				Header: msg.Header(),
+				Body:   msg.Body(),
+			}
+			dlMsg.Header["error"] = err.Error()
+			topic := msg.Topic() + "DLQ"
+			if err := w.b.Publish(topic, dlMsg); err != nil {
+				logger.Errorf("failed to publish to %s, error: %s", topic, err.Error())
+			}
+			logger.Info("死信队列结束")
 			return nil
 		}
 	}
