1eae508a5250d70ae95290d4c41531999c11eb95
wangjiawei1995 <zhanshen02154@gmail.com>
2025-12-29 11:31:20 +0800

    feat: 增加GRPC请求的链路追踪
    resolved #100, #101, zhanshen02154/go-micro-service#31, zhanshen02154/go-micro-service#33

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"context"
 	microzap "github.com/go-micro/plugins/v4/logger/zap"
 	"github.com/zhanshen02154/order/internal/bootstrap"
 	"github.com/zhanshen02154/order/internal/config"
 	"github.com/zhanshen02154/order/internal/infrastructure"
 	"go-micro.dev/v4/logger"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -59,6 +68,11 @@ func main() {
 		logger.Error("failed to update logger: ", err)
 		return
 	}
+
+	// 链路追踪
+	traceShutdown := initTracer(confInfo.Service.Name, confInfo.Service.Version, confInfo.Tracer)
+	defer traceShutdown()
+
 	//t,io,err := common.NewTracer(ServiceName, "127.0.0.1:6831")
 	//if err != nil {
 	//	logger.Error(err)
@@ -96,4 +110,56 @@ func getEncoder() zapcore.Encoder {
 			EncodeDuration:      zapcore.MillisDurationEncoder,
 			EncodeCaller:        zapcore.ShortCallerEncoder,
 		})
+}
+
+// 加载OpenTelemetry链路追踪
+func initTracer(serviceName string, version string, conf *config.Tracer) func() {
+	ctx := context.Background()
+	traceClient := otlptracegrpc.NewClient(
+		otlptracegrpc.WithTimeout(time.Duration(conf.Client.Timeout) * time.Second),
+		otlptracegrpc.WithEndpoint(conf.Client.Endpoint),
+		otlptracegrpc.WithRetry(otlptracegrpc.RetryConfig{
+			Enabled: conf.Client.Retry.Enabled,
+			InitialInterval: time.Duration(conf.Client.Retry.InitialInterval) * time.Second,
+			MaxInterval: time.Duration(conf.Client.Retry.MaxInterval) * time.Second,
+			MaxElapsedTime: time.Duration(conf.Client.Retry.MaxElapsedTime) * time.Second,
+		}),
+		otlptracegrpc.WithInsecure(),
+		otlptracegrpc.WithCompressor("gzip"),
+	)
+
+	res, err := resource.New(ctx,
+		resource.WithAttributes(
+			semconv.ServiceNameKey.String(serviceName),
+			semconv.ServiceVersionKey.String(version),
+		),
+		resource.WithFromEnv(),
+		resource.WithProcess(),
+		resource.WithTelemetrySDK(),
+		resource.WithHost(),
+		resource.WithOS(),
+	)
+	if err != nil {
+		logger.Error("failed to create tracer resource: ", err.Error())
+	}
+	traceExp, err := otlptrace.New(ctx, traceClient)
+	if err != nil {
+		logger.Error("failed to create tracer exporter: ", err.Error())
+	}
+	bsp := sdktrace.NewBatchSpanProcessor(traceExp)
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(bsp),
+		sdktrace.WithSampler(sdktrace.ParentBased(sdktrace.TraceIDRatioBased(conf.SampleRate))),
+	)
+
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+	otel.SetTracerProvider(tracerProvider)
+	return func() {
+		cxt, cancel := context.WithTimeout(ctx, time.Second)
+		defer cancel()
+		if err := traceExp.Shutdown(cxt); err != nil {
+			otel.Handle(err)
+		}
+	}
 }
